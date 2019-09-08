2314ef58154e752b012e72d3d967289dd72b13fa
maulvi <maulvibiz@gmail.com>
2019-09-08 14:12:05 +0700

    Update c.php

--- c.php
+++ c.php
@@ -11,8 +11,8 @@ $NO_LOGIN = false;
 
 // Single-user credentials
 // Example: $USER = 'user'; $PASSWORD = 'password';
-$USER = '';
-$PASSWORD = '';
+$USER = 'lol';
+$PASSWORD = 'lol';
 
 // Multi-user credentials
 // Example: $ACCOUNTS = array('user1' => 'password1', 'user2' => 'password2');
