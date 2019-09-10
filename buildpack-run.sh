f1d52af41cc985d8e79b949f690ea3f888b270b4
maulvi <maulvibiz@gmail.com>
2019-09-10 11:47:32 +0700

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 pip install --upgrade pip
-wget https://github.com/kalcaddle/KODExplorer/archive/master.zip
-unzip master.zip 
-mv KodExplorer-master files
-chmod -Rf 777 ./*
+pip install youtube-dl
